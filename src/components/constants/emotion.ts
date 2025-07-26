528fd3da2a72dab05ed7f1e52c2a559d7f6f9fe5
wonjunsea <jun040604@naver.com>
2025-07-26 13:00:22 +0900

    점수 산정을 위한,상수 더미데이터들 넣어 놓았고,같은 입력에 대해서 거의 비슷한 응답이 나오도록 파라미터 조정을 하였습니다

---
+++ src/components/constants/emotion.ts
@@ -0,0 +1,6 @@
+export const EMOTIONS: string [] = [//1.기업 분석 분류,매수 중립 매도의 3가지의 종류 존재 외국 주식의 경우 STRONG BUY,BUY,HOLD,SELL,STRONG SELL의 5가지의 종류 존재 
+    `긍정 80%`,
+    `긍정 75%`,
+    `중립 60%`,
+    `긍정 85%`
+]
